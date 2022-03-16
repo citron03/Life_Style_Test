d0d9720c12f205cc2c1934e5bc95085a8793395c
citron03 <cchan0617@gmail.com>
2022-03-16 14:28:28 +0900

    오타 수정

--- src/component/Result.jsx
+++ src/component/Result.jsx
@@ -43,7 +43,7 @@ const Result = ( {nature, density, setPage, initialization} ) => {
             objectType: "feed",
             content: {
                 title: result[1],
-                description: "Life Style Place의 결과입니다.",
+                description: "Life Style Place Test",
                 imageUrl: "https://pixabay.com/get/gb3e0436ab09cbcffdaebaaa49a5a8af44f07a498b3a04d47529b6a77972ebaec16ba33ca189363f90b397a39e30dc538bbe9ae77f5de1c33f258e4c77f1f1d4bc3753fae7d2407ce6a6a785c409c4628_1280.png",
                 link: {
                 mobileWebUrl: window.location.href,
